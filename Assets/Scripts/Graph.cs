bdcf7d19e45404667a0f8cb9fd1273b32c931c64
integerincrement <87837473+integerincrement@users.noreply.github.com>
2024-04-23 16:35:07 -0400

    added shader graph files and finalized script

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -1,27 +1,39 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Graph : MonoBehaviour {
 
     [SerializeField]
     Transform pointPrefab;
 
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
+
+    Transform[] points;
+
     private void Awake()
     {
-        for (int i = 0; i < 10; i++)
-        {
-            Transform point = Instantiate(pointPrefab);
-            point.localPosition = Vector3.right * ((i + 0.5f) /5f - 1f);
-            point.localScale = Vector3.one / 5f;
+        float step = 2f / resolution;
+        Vector3 scale = Vector3.one * step;
+        Vector3 position = Vector3.zero;
 
+        points = new Transform[resolution] ;
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            
+            Transform point = points[i] = Instantiate(pointPrefab);
+            
+            position.x = (i + 0.5f) * step - 1f;
+            //position.y = (1/2f) * position.x + 2;
+            //line of the form y = mx + b
+
+            point.localPosition = position;
+            point.localScale = scale;
+
+            point.SetParent(transform, false);
 
         }
-
-
-
-
-
-
-
 
         ///////////////////////////////////////
         //point.localPosition = Vector3.right;/
@@ -29,5 +41,20 @@ public class Graph : MonoBehaviour {
         //point = Instantiate (pointPrefab);///
         //point.localPosition=Vector3.right*2f;
         ///////////////////////////////////////
+    }
+
+    void Update()   {
+
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++)
+        {
+
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
+
+        }
+        
     }
 }
